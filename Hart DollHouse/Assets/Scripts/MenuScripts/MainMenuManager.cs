e18dfd08d3ede1bbd37d9d470d652a32d0a997d9
SusterDrifter <39788874+SusterDrifter@users.noreply.github.com>
2018-06-27 17:03:32 +0800

    disabled continue and scene selection button
    when no progress id done by player

--- Hart DollHouse/Assets/Scripts/MenuScripts/MainMenuManager.cs
+++ Hart DollHouse/Assets/Scripts/MenuScripts/MainMenuManager.cs
@@ -1,20 +1,27 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class MainMenuManager : MonoBehaviour {
 
+    private Sound button;
+
     [SerializeField] private Button continueButton;
     [SerializeField] private Button sceneSelectButton;
-    private Sound button;
+    [SerializeField] private TextMeshProUGUI continueText;
+    [SerializeField] private TextMeshProUGUI sceneSelectText;
 
     private void Awake()
     {
-        int playerProgress = GameManager.instance.currentSceneIndex;
+        int playerProgress = PlayerPrefs.GetInt(GameManager.instance.PlayerSceneStr);
+
         if (playerProgress <= 1)
         {
             continueButton.interactable = false;
             sceneSelectButton.interactable = false;
+            continueText.alpha = 0.2f;
+            sceneSelectText.alpha = 0.2f;
         }
     }
 
