94b2736c51291173d7796749cf80db81e42a9b4b
SusterDrifter <39788874+SusterDrifter@users.noreply.github.com>
2018-07-15 00:13:30 +0800

    fixed audioable object, added animations and sfx

--- Hart DollHouse/Assets/Scripts/GeneralScripts/ItemScripts/AudioableObject.cs
+++ Hart DollHouse/Assets/Scripts/GeneralScripts/ItemScripts/AudioableObject.cs
@@ -2,56 +2,43 @@
 
 public class AudioableObject : DialogueObject {
 
-    public Sound viableSound;
+    public Sound viableSoundOn;
+    public Sound viableSoundOff;
     public Sound nonviableSound;
-    private AudioSource source;
 
-    private void Awake()
+    private bool isOn = false;
+
+    private void InitialiseSound(Sound sound)
     {
-        if (viableSound.clip != null || nonviableSound.clip != null)
-            source = gameObject.AddComponent<AudioSource>();
-
-        if (viableSound.clip != null)
+        if (sound.clip != null && sound.source == null)
         {
-            viableSound.source = source;
-            viableSound.source.clip = viableSound.clip;
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
 
-            viableSound.source.volume = viableSound.volume;
-            viableSound.source.pitch = viableSound.pitch;
-            viableSound.source.spatialBlend = viableSound.spatialBlend;
+            sound.source.volume = sound.volume;
+            sound.source.pitch = sound.pitch;
+            sound.source.spatialBlend = sound.spatialBlend;
 
-            viableSound.source.loop = viableSound.loop;
-            viableSound.source.playOnAwake = viableSound.playOnAwake;
-        }
-
-        if (nonviableSound.clip != null)
-        {
-            nonviableSound.source = source;
-            nonviableSound.source.clip = nonviableSound.clip;
-
-            nonviableSound.source.volume = nonviableSound.volume;
-            nonviableSound.source.pitch = nonviableSound.pitch;
-            nonviableSound.source.spatialBlend = nonviableSound.spatialBlend;
-
-            nonviableSound.source.loop = nonviableSound.loop;
-            nonviableSound.source.playOnAwake = nonviableSound.playOnAwake;
+            sound.source.loop = sound.loop;
+            sound.source.playOnAwake = sound.playOnAwake;
         }
     }
 
     public override void Interact()
     {
         base.Interact();
-        if (viableSound.clip != null && base.isViable)
-        {
-            PlayClip(viableSound);
-        } else if (nonviableSound.clip != null && !base.isViable)
-        {
-            PlayClip(nonviableSound);
-        }
+        // If not viable and has nonviable sound
+        if (!base.isViable) { PlayClip(nonviableSound); }
+        // If viable and off and has viable on sound
+        else if (base.isViable && !isOn) { PlayClip(viableSoundOn); isOn = true; }
+        // If viable and on and has viable off sound
+        else if (base.isViable && isOn) { PlayClip(viableSoundOff); isOn = false; }
     }
 
     private void PlayClip(Sound sound)
     {
-        sound.source.Play();
+        InitialiseSound(sound);
+        if (sound.clip != null)
+            sound.source.Play();
     }
 }
