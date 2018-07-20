a248c36238020fe827b8b38438c82568e8c5aaf6
SusterDrifter <39788874+SusterDrifter@users.noreply.github.com>
2018-07-20 11:47:05 +0800

    added chapter 1.2

--- Hart DollHouse/Assets/Scripts/Chapter1_2/Chapter1_2.cs
+++ Hart DollHouse/Assets/Scripts/Chapter1_2/Chapter1_2.cs
@@ -1,15 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Chapter1_2 : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    [SerializeField] private Dialogue startDiag;
+    [SerializeField] private Dialogue endingDiag;
+    [SerializeField] private Dialogue ending2Diag;
+    [SerializeField] private Sound scribble;
+
+    void Start () {
+        if (scribble.clip)
+        {
+            scribble.source = gameObject.AddComponent<AudioSource>();
+            scribble.source.clip = scribble.clip;
+
+            scribble.source.volume = scribble.volume;
+            scribble.source.pitch = scribble.pitch;
+            scribble.source.spatialBlend = scribble.spatialBlend;
+
+            scribble.source.loop = scribble.loop;
+            scribble.source.playOnAwake = scribble.playOnAwake;
+        }
+            
 	}
 	
-	// Update is called once per frame
+    public void StartDiag()
+    {
+        MainUIManager.instance.GetDialogueUIManager().GetManager().BeginDialogue(startDiag);
+    }
+
+    public void EndDiag()
+    {
+        MainUIManager.instance.GetDialogueUIManager().GetManager().BeginDialogue(endingDiag);
+    }
+
+    public void EndTwoDiag()
+    {
+        MainUIManager.instance.GetDialogueUIManager().GetManager().BeginDialogue(ending2Diag);
+    }
+
+    public void ScribbleSound()
+    {
+        AudioManager.instance.PlayClip(scribble);
+    }
+
 	void Update () {
 		
 	}
