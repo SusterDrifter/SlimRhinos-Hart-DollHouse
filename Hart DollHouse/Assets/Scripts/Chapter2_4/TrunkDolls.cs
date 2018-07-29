60cc75350d48be1c8b9c97dd7ea98807e3e01d97
SusterDrifter <39788874+SusterDrifter@users.noreply.github.com>
2018-07-29 22:59:53 +0800

    minor addition to 2.4

---
+++ Hart DollHouse/Assets/Scripts/Chapter2_4/TrunkDolls.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class TrunkDolls : DialogueObject {
+
+    [SerializeField] private Sound breakingSound;
+
+    public override void Interact()
+    {
+        base.Interact();
+        if (breakingSound.clip)
+            Chapter2_4.instance.DeactivateTransition(this.gameObject, breakingSound);
+    }
+}
